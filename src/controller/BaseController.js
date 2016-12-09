02b0039bf73fef4deeeaa6547cc9e69011c9a757
unknown <shilong.sang@qunarservers.com>
2016-12-09 11:51:14 +0800

    refact

--- src/controller/BaseController.js
+++ src/controller/BaseController.js
@@ -1,11 +1,11 @@
 'use strict';
 
-module.exports = class ApiController {
+module.exports = class BaseController {
   constructor(ctx, next) {
     this.ctx = ctx
     this.query = ctx.query
     this.next = next
 
-    this.global_filter = ['koa-bodyparser', 'koa-views']
+    this.global_filter = ['koa-bodyparser']
   }
 }
