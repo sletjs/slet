d6c222d6c8e61dc1006f0e590e5e4dc0827dddde
i5ting <i5ting@126.com>
2016-12-12 13:23:55 +0800

    refact, please ignore

--- test/dept/base.js
+++ test/dept/base.js
@@ -1,36 +1,18 @@
 import test from 'ava'
- 
-// ### 第一种，暴露path和controller
-// （controller内部不需要path）
 
-// ```
-// app.router('/', require('./ctrl') )  
-// ```
-
-// or
-
-// ```
-// app.router('/', './ctrl')  
-// ```
-const axios = require('axios')
 const Slet = require('../..');
 const app = new Slet({
     root: __dirname,
-    debug: false,
-    "views" :{
-        "path" : __dirname + '/fixtures',
-        "option": { "map": {"html": "nunjucks" }}
-    }
+    debug: false
 });
 
-test.cb('GET　/r?a=１ return json', t => {
+test('app.identifyDept()', t => {
   return app.identifyDept().then(function(r){
-    
-    console.log("r")
-    
-    // t.true(app.scanedControllerArray.length > 0)
 
-    t.pass();
-    t.end() 
+    // console.log(r)
+    t.true(r.length > 0)
+    t.true(app.scanedControllerArray.length > 0)
+
+    t.pass()
   })
 })
