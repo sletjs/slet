84646b272fc22151f16a7f68926c23da59ba07c9
i5ting <i5ting@126.com>
2016-12-28 09:16:52 +0800

    lazycommmit, please ignore

--- packages/slet-basecontroller/test/req/req.xhr.js
+++ packages/slet-basecontroller/test/req/req.xhr.js
@@ -4,23 +4,6 @@ var sletTest = require('slettest')
 const Slet = require('../../../slet')
 const BaseController = require('../..')
 
-// it('should return true when X-Requested-With is xmlhttprequest', function(done){
-//   var app = express();
-//
-//   app.use(function(req, res){
-//     req.xhr.should.be.true;
-//     res.end();
-//   });
-//
-//   request(app)
-//   .get('/')
-//   .set('X-Requested-With', 'xmlhttprequest')
-//   .expect(200)
-//   .end(function(err, res){
-//     done(err);
-//   })
-// })
-
 test.cb('.xhr should return true when X-Requested-With is xmlhttprequest', t => {
   const app = new Slet({
     root: __dirname,
