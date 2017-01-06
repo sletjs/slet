1d780eaa903f9cfb93898bcac90f8fb02f8de371
i5ting <i5ting@126.com>
2017-01-06 10:01:02 +0800

    refact, please ignore

--- app.js
+++ app.js
@@ -5,43 +5,14 @@ const Slet = require('./packages/slet')
 const app = new Slet()
 
 class MyController extends Slet.BaseController {
-  get (req, res) { 
-    req.accepts(['json']);
-    var a = req.query.a
-    // res.status = 200
-    return res.send({a:1})
+  constructor (app, ctx, next) {
+    super(app, ctx, next)
 
-    //  res.locals.user = {a:1}
-    // return res.body = `hello world ${a}`
-    // res.send(`hello world ${a}`)
-    // return res.download('/lerna.json')
-    // res.status = 201
-    // return res.sendStatus(200)
-
-    return res.format({
-      'text/plain': function(){
-        res.send('hey');
-      },
-
-      'text/html': function(){
-        res.send('<p>hey</p>');
-      },
-
-      'application/json': function(){
-        res.send({ message: 'hey' });
-      },
-
-      'default': function() {
-        // log the request and respond with 406
-        res.status(406).send('Not Acceptable');
-      }
-    });
-
-    return res.json({a:1})
-    return res.jsonp(null)
-    return res.location('/foo/bar');
-
-    // return res.setStatus(201).end('sss')
+    this.path = '/'
+  }
+  
+  get (req, res) {
+    return ({ hello: 'world' })
   }
   
   post(req, res) {
@@ -51,6 +22,8 @@ class MyController extends Slet.BaseController {
   } 
 }
 
-app.router('/:id', MyController)
+app.router(MyController)
 
 app.start(3000) 
+
+module.exports = app.callback()
